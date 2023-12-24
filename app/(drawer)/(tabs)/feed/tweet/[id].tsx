3bac595959f6d85ae8ee12cc22af1e4dabd729c6
AbdulMateenzwl <abdulmateenkhanzwl@gmail.com>
2023-12-24 22:49:55 +0500

    Update tanstack dependencies and add tweet API calls

--- app/(drawer)/(tabs)/feed/tweet/[id].tsx
+++ app/(drawer)/(tabs)/feed/tweet/[id].tsx
@@ -1,16 +1,25 @@
-import { Text } from 'react-native';
+import { ActivityIndicator, Text } from 'react-native';
 import Tweet from '../../../../../components/Tweet';
 import tweets from '../../../../../assets/data/tweets';
 import { useSearchParams } from 'expo-router';
+import { useQuery } from '@tanstack/react-query';
+import { getTweet } from '../../../../../lib/api/tweets';
 
 export default function TweetScreen() {
-  const { id } = useSearchParams();
+	const { id } = useSearchParams();
 
-  const tweet = tweets.find((t) => t.id === id);
+	const { data, isLoading, error } = useQuery({
+		queryKey: ['tweet', id],
+		queryFn: () => getTweet(id as string),
+	});
 
-  if (!tweet) {
-    return <Text>Tweet {id} not found!</Text>;
-  }
+	if (isLoading) {
+		return <ActivityIndicator />;
+	}
 
-  return <Tweet tweet={tweet} />;
+	if (error) {
+		return <Text>Error: {error.message}</Text>;
+	}
+
+	return <Tweet tweet={data} />;
 }
--- app/new-tweet.tsx
+++ app/new-tweet.tsx
@@ -1,92 +1,116 @@
 import { useState } from 'react';
 import { Link, useRouter } from 'expo-router';
 import {
-  View,
-  StyleSheet,
-  Text,
-  Image,
-  TextInput,
-  Pressable,
-  SafeAreaView,
+	View,
+	StyleSheet,
+	Text,
+	Image,
+	TextInput,
+	Pressable,
+	SafeAreaView,
+	ActivityIndicator,
 } from 'react-native';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { createTweet } from '../lib/api/tweets';
 
 const user = {
-  id: 'u1',
-  username: 'VadimNotJustDev',
-  name: 'Vadim',
-  image:
-    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png',
+	id: 'u1',
+	username: 'VadimNotJustDev',
+	name: 'Vadim',
+	image:
+		'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.png',
 };
 
 export default function NewTweet() {
-  const [text, setText] = useState('');
-  const router = useRouter();
+	const [text, setText] = useState('');
+	const router = useRouter();
 
-  const onTweetPress = () => {
-    console.warn('Positng the tweet: ', text);
+	const queryClient = useQueryClient();
 
-    setText('');
-    router.back();
-  };
+	const { mutateAsync, isLoading, isError, error } = useMutation({
+		mutationFn: createTweet,
+		onSuccess: (data) => {
+			// queryClient.invalidateQueries({ queryKey: ['tweets'] })
+			queryClient.setQueriesData(['tweets'], (existingTweets: any) => {
+				return [data, ...existingTweets];
+			});
+		},
+	});
 
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
-      <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <Link href="../" style={{ fontSize: 18 }}>
-            Cancel
-          </Link>
+	const onTweetPress = async () => {
+		console.warn('Posting the tweet: ', text);
 
-          <Pressable onPress={onTweetPress} style={styles.button}>
-            <Text style={styles.buttonText}>Tweet</Text>
-          </Pressable>
-        </View>
+		try {
+			await mutateAsync({ content: text });
+			setText('');
+			router.back();
+		} catch (error) {
+			console.log('Error :', (error as Error).message);
+		}
+	};
 
-        <View style={styles.inputContainer}>
-          <Image src={user.image} style={styles.image} />
-          <TextInput
-            value={text}
-            onChangeText={setText}
-            placeholder="What's happening?"
-            multiline
-            numberOfLines={5}
-            style={{ flex: 1 }}
-          />
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+			<View style={styles.container}>
+				<View style={styles.buttonContainer}>
+					<Link href='../' style={{ fontSize: 18 }}>
+						Cancel
+					</Link>
+					{isLoading && <ActivityIndicator />}
+					<Pressable onPress={onTweetPress} style={styles.button}>
+						<Text style={styles.buttonText}>Tweet</Text>
+					</Pressable>
+				</View>
+
+				<View style={styles.inputContainer}>
+					<Image src={user.image} style={styles.image} alt='User Image' />
+					<TextInput
+						value={text}
+						onChangeText={setText}
+						placeholder="What's happening?"
+						multiline
+						numberOfLines={5}
+						style={{ flex: 1 }}
+					/>
+				</View>
+
+				{isError && (
+					<Text style={{ color: 'red' }}>{(error as Error).message}</Text>
+				)}
+			</View>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    flex: 1,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    marginVertical: 10,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: '#1C9BF0',
-    padding: 10,
-    paddingHorizontal: 20,
-    borderRadius: 50,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: '600',
-    fontSize: 16,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-  },
-  image: {
-    width: 50,
-    aspectRatio: 1,
-    borderRadius: 50,
-    marginRight: 10,
-  },
+	container: {
+		padding: 10,
+		flex: 1,
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		marginVertical: 10,
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	button: {
+		backgroundColor: '#1C9BF0',
+		padding: 10,
+		paddingHorizontal: 20,
+		borderRadius: 50,
+	},
+	buttonText: {
+		color: 'white',
+		fontWeight: '600',
+		fontSize: 16,
+	},
+	inputContainer: {
+		flexDirection: 'row',
+	},
+	image: {
+		width: 50,
+		aspectRatio: 1,
+		borderRadius: 50,
+		marginRight: 10,
+	},
 });
