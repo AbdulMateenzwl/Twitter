a41ca9c3684a3dba9a5750cf6b69bf23f57f7393
AbdulMateenzwl <abdulmateenkhanzwl@gmail.com>
2023-12-25 13:44:48 +0500

    Refactor AuthContext and TweetsApiContext

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,51 +1,58 @@
 import { useRouter, useSegments } from 'expo-router';
 import {
-  PropsWithChildren,
-  createContext,
-  useState,
-  useContext,
-  useEffect,
+	PropsWithChildren,
+	createContext,
+	useState,
+	useContext,
+	useEffect,
 } from 'react';
 import * as SecureStore from 'expo-secure-store';
 
 const AuthContext = createContext({});
 
 const AuthContextProvider = ({ children }: PropsWithChildren) => {
-  const [authToken, setAuthToken] = useState<string | null>(null);
-  const segments = useSegments();
-  const router = useRouter();
+	const [authToken, setAuthToken] = useState<string | null>(null);
+	const segments = useSegments();
+	const router = useRouter();
 
-  useEffect(() => {
-    const isAuthGroup = segments[0] === '(auth)';
+	useEffect(() => {
+		const isAuthGroup = segments[0] === '(auth)';
 
-    if (!authToken && !isAuthGroup) {
-      router.replace('/signIn');
-    }
-    if (authToken && isAuthGroup) {
-      router.replace('/');
-    }
-  }, [segments, authToken]);
+		if (!authToken && !isAuthGroup) {
+			router.replace('/signIn');
+		}
+		if (authToken && isAuthGroup) {
+			router.replace('/');
+		}
+	}, [segments, authToken]);
 
-  useEffect(() => {
-    const loadAuthToken = async () => {
-      const res = await SecureStore.getItemAsync('authToken');
-      if (res) {
-        setAuthToken(res);
-      }
-    };
-    loadAuthToken();
-  }, []);
+	useEffect(() => {
+		const loadAuthToken = async () => {
+			const res = await SecureStore.getItemAsync('authToken');
+			if (res) {
+				setAuthToken(res);
+			}
+		};
+		loadAuthToken();
+	}, []);
 
-  const updateAuthToken = async (newToken: string) => {
-    await SecureStore.setItemAsync('authToken', newToken);
-    setAuthToken(newToken);
-  };
+	const updateAuthToken = async (newToken: string) => {
+		await SecureStore.setItemAsync('authToken', newToken);
+		setAuthToken(newToken);
+	};
 
-  return (
-    <AuthContext.Provider value={{ authToken, updateAuthToken }}>
-      {children}
-    </AuthContext.Provider>
-  );
+	const removeAuthToken = async () => {
+		await SecureStore.deleteItemAsync('authToken');
+		setAuthToken(null);
+	};
+
+	return (
+		<AuthContext.Provider
+			value={{ authToken, updateAuthToken, removeAuthToken }}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 export default AuthContextProvider;
